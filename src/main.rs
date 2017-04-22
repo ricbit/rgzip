16ff897a5f23354e5e9a1dc83187b8fe513163c7
Ricardo Bittencourt <bluepenguin@gmail.com>
2017-04-22 12:08:16 -0300

    Refactor ByteSource

--- src/main.rs
+++ src/main.rs
@@ -44,19 +44,20 @@ impl fmt::Display for GzipError {
 trait ByteSource {
     fn get_u8(&mut self) -> Result<u8, GzipError>;
 
-    fn get_u16(&mut self) -> Result<u16, GzipError> {
-        let mut ans : u16 = 0;
-        for i in 0..2 {
-            ans |= (try!(self.get_u8()) as u16) << (8 * i);
-        }
-        Ok(ans)
-    }
-    fn get_u32(&mut self) -> Result<u32, GzipError> {
+    fn get_variable(&mut self, size: u8) -> Result<u32, GzipError> {
         let mut ans : u32 = 0;
-        for i in 0..4 {
+        for i in 0..size {
             ans |= (try!(self.get_u8()) as u32) << (8 * i);
         }
         Ok(ans)
+    }
+
+    fn get_u16(&mut self) -> Result<u16, GzipError> {
+        self.get_variable(2).map(|x| x as u16)
+    }
+
+    fn get_u32(&mut self) -> Result<u32, GzipError> {
+        self.get_variable(4)
     }
 }
 
